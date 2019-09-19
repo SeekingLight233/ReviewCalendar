e03ebdf26cea963f62bedd360a10f17dd03dbdbb
PraiseSunAsh <690163223@qq.com>
2019-09-19 18:42:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <mu-paper class="demo-paper" :z-depth="4" id="mypaper">
+      <div class="title">
+        
+      </div>
+      <mu-avatar slot="avatar" size=80 id="myava">
+        <img src="./assets/icon.jpg" />
+      </mu-avatar>
+      <mu-form ref="form" :model="validateForm" class="mu-demo-form" id="myform">
+        <mu-form-item label="用户名" help-text prop="username" :rules="usernameRules">
+          <mu-text-field v-model="validateForm.username" prop="username"></mu-text-field>
+        </mu-form-item>
+        <mu-form-item label="密码" prop="password" :rules="passwordRules">
+          <mu-text-field type="password" v-model="validateForm.password" prop="password"></mu-text-field>
+        </mu-form-item>
+        <mu-form-item>
+          <mu-button color="primary" @click="submit" id="mybutton">登录</mu-button>
+          <mu-button @click="clear">注册</mu-button>
+        </mu-form-item>
+      </mu-form>
+    </mu-paper>
+
+    <!-- <router-view/> -->
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      usernameRules: [
+        { validate: val => !!val, message: "必须填写用户名" },
+        { validate: val => val.length >= 3, message: "用户名长度大于3" }
+      ],
+      passwordRules: [
+        { validate: val => !!val, message: "必须填写密码" },
+        {
+          validate: val => val.length >= 3 && val.length <= 10,
+          message: "密码长度大于3小于10"
+        }
+      ],
+      argeeRules: [{ validate: val => !!val, message: "必须同意用户协议" }],
+      validateForm: {
+        username: "",
+        password: "",
+        isAgree: false
+      }
+    };
+  },
+  methods: {
+    submit() {
+      this.$refs.form.validate().then(result => {
+        console.log("form valid: ", result);
+      });
+    },
+    clear() {
+      this.$refs.form.clear();
+      this.validateForm = {
+        username: "",
+        password: "",
+        isAgree: false
+      };
+    }
+  }
+};
+</script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -27,5 +84,45 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+.mu-demo-form {
+  width: 100%;
+  max-width: 100%;
+}
+#myform {
+  /* margin-right: 0px; */
+  background: #f5eeee;
+  
+}
+#mypaper {
+  width: 36%;
+  margin: 200px auto;
+  border-radius: 10px;
+   background: #f5eeee;
+}
+#mybutton {
+  margin-right: 20%;
+  margin-left: 24%;
+}
+#myava{
+  margin-bottom: 0px;
+}
+.title{
+  background: url('./assets/text.png') no-repeat;
+  /* border: 2px solid red; */
+  background-size: 100%;
+  width: 100%;
+  height: 150px;
+}
+@media screen and (max-width: 600px) {
+  #mypaper {
+    width: 100%;
+    margin: 100px auto;
+    border-radius: 10px;
+  }
+  #mybutton {
+    margin-right: 11%;
+    margin-left: 20%;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'muse-ui/lib/styles/base.less';
+import { Button, Select,Form, TextField, Paper,Avatar} from 'muse-ui';
+import 'muse-ui/lib/styles/theme.less';
+import 'muse-ui/dist/muse-ui.css';
+import theme from 'muse-ui/lib/theme';
 
+// theme.use('dark');
 Vue.config.productionTip = false
+
+Vue.use(Button);
+Vue.use(Select);
+Vue.use(Form);
+Vue.use(TextField);
+Vue.use(Paper);
+Vue.use(Avatar);
 
 new Vue({
   router,
