ae93194753b464689a214f114840cf8b4093bb60
PraiseSunAsh <690163223@qq.com>
2019-11-06 22:36:37 +0800

    append

--- src/main.js
+++ src/main.js
@@ -5,18 +5,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'muse-ui/lib/styles/base.less';
-import { Button, Select,Form, TextField, Paper,Avatar} from 'muse-ui';
+import { Button, Select, Form, TextField, Paper, Avatar } from 'muse-ui';
 import 'muse-ui/lib/styles/theme.less';
 import 'muse-ui/dist/muse-ui.css';
-import { Calendar,Loading,MessageBox,} from 'element-ui';
+import { Calendar, Loading, MessageBox, } from 'element-ui';
 import theme from 'muse-ui/lib/theme';
 import * as colors from 'muse-ui/lib/theme/colors';
 import axios from '../node_modules/axios';
+import 'vue-event-calendar/dist/style.css' //1.1.10之后的版本，css被放在了单独的文件中，方便替换
+import vueEventCalendar from 'vue-event-calendar'
 
+Vue.use(vueEventCalendar, { locale: 'en' }) //可以设置语言，支持中文和英文
 
 theme.add('custom-theme', {
-  primary: colors.redA200,
-  secondary: colors.pinkA200
+    primary: colors.redA200,
+    secondary: colors.pinkA200
 });
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
@@ -30,8 +33,8 @@ Vue.use(Calendar);
 Vue.use(Loading.directive);
 
 new Vue({
-  router,
-  store,
-  axios,
-  render: h => h(App)
+    router,
+    store,
+    axios,
+    render: h => h(App)
 }).$mount('#app')
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,7 +1,120 @@
 <template>
   <div class="about">
     <h1>这里是用户界面</h1>
-    
-     <mu-button color="primary">Primary</mu-button>
+    <vue-event-calendar :events="demoEvents" @dayChanged="dayChanged()"></vue-event-calendar>
+    <mu-button color="primary">Primary</mu-button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      demoEvents: [
+        {
+          date: "2016/11/12", // 必填
+          title: "Foo" // 必填
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+        {
+          date: "2016/12/15",
+          title: "Bar",
+          desc: "description",
+          customClass: "disabled highlight" // 自定义日历单元格的Class，会加到对应的日历单元格上
+        },
+      ]
+    };
+  },
+  methods: {
+    monthChange(month) {
+      console.log(month);
+    },
+    dayChange(day) {
+      console.log(day);
+    }
+  }
+};
+</script>
